840836653e9aba992b9acbe2adfe9d382040ec47
Milanrdnvc <milanradinovic@rocketmail.com>
2020-10-15 20:00:59 +0200

    Added content for products

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import pillow1 from '../Pictures/Pillow1.jpg';
+import pillow2 from '../Pictures/Pillow2.jpg';
+import pillow3 from '../Pictures/Pillow3.jpg';
+import pillow4 from '../Pictures/Pillow4.jpg';
+import pillow5 from '../Pictures/Pillow5.jpg';
 import '../CSS/Products.css';
 
 function Products() {
@@ -8,31 +12,38 @@ function Products() {
       <h1>Our Products</h1>
       <div className="products-grid">
         <div className="products-grid__product">
-          <img src={pillow1} alt="product-1" width="350px" />
+          <img src={pillow1} alt="product-1" width="300px" height="300px" />
           <div className="products-grid__text">
-            <h3>Product Name</h3>
-            <h3>10000$</h3>
+            <h3>02 Body Pillow</h3>
+            <h3>20$</h3>
           </div>
         </div>
         <div className="products-grid__product">
-          <img src={pillow1} alt="product-1" width="350px" />
+          <img src={pillow2} alt="product-1" width="300px" height="300px" />
           <div className="products-grid__text">
-            <h3>Product Name</h3>
-            <h3>10000$</h3>
+            <h3>Rem Body Pillow</h3>
+            <h3>60$</h3>
           </div>
         </div>
         <div className="products-grid__product">
-          <img src={pillow1} alt="product-1" width="350px" />
+          <img src={pillow3} alt="product-1" width="300px" height="300px" />
           <div className="products-grid__text">
-            <h3>Product Name</h3>
-            <h3>10000$</h3>
+            <h3>Astolfo Body Pillow</h3>
+            <h3>35$</h3>
           </div>
         </div>
         <div className="products-grid__product">
-          <img src={pillow1} alt="product-1" width="350px" />
+          <img src={pillow4} alt="product-1" width="300px" height="300px" />
           <div className="products-grid__text">
-            <h3>Product Name</h3>
-            <h3>10000$</h3>
+            <h3>Rias Body Pillow</h3>
+            <h3>53$</h3>
+          </div>
+        </div>
+        <div className="products-grid__product">
+          <img src={pillow5} alt="product-1" width="300px" />
+          <div className="products-grid__text">
+            <h3>Megumin Body Pillow</h3>
+            <h3>28$</h3>
           </div>
         </div>
       </div>
